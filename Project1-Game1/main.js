2dcd71e25fe480b7062566407291aef87ae91a96
Cristina Ruiz <cruiz731@gmail.com>
2016-06-30 11:53:28 -0400

    update players turn indicator is just a change in bg color, shows hit or miss header after each click, game ends when number of hits is 9

--- Project1-Game1/main.js
+++ Project1-Game1/main.js
@@ -1,11 +1,12 @@
-/*Testing hardcoiding ship positions*/
+/*Testing hardcoding ship positions*/
+alert("Starting Game, go Player 1");
 
 var shipsPlayer1 = [9,10,11,12,21,29,37,50,51]; // each index references gridCells array
 var shipsPlayer2 = [2,3,15,23,31,58,59,60,61];
 var gridCells = ['A1', 'A2', 'A3', 'A4', 'A5', 'A6', 'A7', 'A8', 'B1', 'B2', 'B3', 'B4', 'B5', 'B6', 'B7', 'B8', 'C1', 'C2','C3','C4', 'C5', 'C6', 'C7', 'C8', 'D1', 'D2', 'D3', 'D4', 'D5', 'D6', 'D7', 'D8', 'E1', 'E2', 'E3', 'E4', 'E5', 'E6', 'E7', 'E8', 'F1', 'F2', 'F3', 'F4', 'F5', 'F6', 'F7', 'F8', 'G1', 'G2', 'G3', 'G4', 'G5', 'G6', 'G7', 'G8', 'H1', 'H2', 'H3', 'H4', 'H5', 'H6', 'H7', 'H8'];
-
+var p1score = 0;
+var p2score = 0;
 var players = [ {id:1},{ id:2} ];
-
 var currentPlayer = players[0].id; //updated in nextplayer function
 // populate the cells with event listeners for both players
 createCells(players[0].id); // send player 1
@@ -31,7 +32,7 @@ function createCells(player) {
     };
   };
 };
-
+//
 // this function stores and sends the mouse event information to other functions
 //
 function store() { // argument passed is the cell clicked  on by player
@@ -40,14 +41,14 @@ function store() { // argument passed is the cell clicked  on by player
   var targetCell = idString[2] + idString[3];
   var elTarget = event.srcElement;
   console.log(elTarget);
-  checkHitOrMiss(currentPlayer, targetCell, idString, elTarget);
+  checkHitOrMiss(targetCell, idString, elTarget);
   this.removeEventListener('click',store); // remove the listener
 
   console.log(idString);
 };
 
 //target parameter = 'A3'
-function checkHitOrMiss(player, cell, elementId, elTarget) {
+function checkHitOrMiss(targetCell, elementId, elTarget) {
   console.log("here is the loop of the opponent ships array");
   console.log("This is the player's clicked cell id :::: " + elementId );
 
@@ -64,7 +65,7 @@ function checkHitOrMiss(player, cell, elementId, elTarget) {
   for (var shipsIndex = 0; shipsIndex < 9; shipsIndex ++ ) {
     var opponentCell = gridCells[opponentShips[shipsIndex]]; // index converted to cell value
     console.log(opponentCell);
-    if (cell === opponentCell) {
+    if (targetCell === opponentCell) {
         //call hit function
         console.log("it's a hit go to hit function....");
         hit(elTarget);
@@ -74,37 +75,82 @@ function checkHitOrMiss(player, cell, elementId, elTarget) {
   console.log("it's a miss go to miss function...");
   miss(elTarget);
 };
-
-function hit(cell) {
+//
+function hit(elTarget) {
   console.log("you reached the hit function");
-  console.log(cell);
-  cell.style.backgroundColor = 'red'; //it's a hit
-  // cell
+  console.log(elTarget);
+  console.log("current player in hit function is " + currentPlayer);
+  elTarget.style.backgroundColor = 'red'; //it's a hit
+  if (currentPlayer == 1) {
+    console.log("YOU ENTERED THE NESTED IF OF HIT FOR PLAYER 1")
+    document.querySelector('#player1status').innerHTML = '<h2>HIT!</h2>';
+    p1score ++;
+    //find the ships cell below opponent p2 grid and style bg red
+    // ****code here *****
+    if (p1score === 9) { // check if game is over
+      gameOver(1);
+    }
+  } else if (currentPlayer == 2) {
+    console.log("YOU ENTERED THE ELSE IF OF HIT FOR PLAYER 2");
+    document.querySelector('#player2status').innerHTML = '<h2>HIT!</h2>';
+    p2score ++;
+    if (p2score === 9) { // check if game is over
+      gameOver(2);
+    }
+    //find the ships cell below opponent p1 grid and style bg red
+    // ****code here *****
+  }
   nextPlayer();
 };
-
-function miss(cell) {
+//
+function miss(elTarget) {
   console.log("you reached the miss function");
-  console.log(cell);
-  cell.style.backgroundColor = 'lightblue'; // it's a miss
+  console.log(elTarget);
+  elTarget.style.backgroundColor = 'black'; // it's a miss
+  if (currentPlayer == 1) {
+    console.log("CURRENT PLAYER IS 1 and p1 status should be MISS");
+    document.querySelector('#player1status').innerHTML = '<h2>MISS!</h2>';
+    //find the ships cell below opponent p2 grid and style bg red
+    // ****code here *****
+  } else if (currentPlayer == 2) {
+    document.querySelector('#player2status').innerHTML = '<h2>MISS!</h2>';
+    //find the ships cell below opponent p1 grid and style bg red
+    // ****code here *****
+  }
   nextPlayer();
 };
-
+//
 function nextPlayer(){
-  players.unshift(players.pop());
-  currentPlayer = players[0].id;
-  console.log(" changed ... currentPlayer: "+currentPlayer)
+  var p2header = document.querySelector('#player2container');
+  var p1header = document.querySelector('#player1container');
+  players.unshift(players.pop()); // swap player 1 for player 2
+  currentPlayer = players[0].id; // at position players index 0
+  if (currentPlayer === 1) {
+    console.log(" current player is 1"); // Testing
+    p2header.style.backgroundColor = 'white';
+    p1header.style.backgroundColor = 'orange';
+  } else if (currentPlayer === 2) {
+    console.log("current player is 2"); // Testing
+    p1header.style.backgroundColor = 'white';
+    p2header.style.backgroundColor = 'orange';
+  }
 };
+function gameOver(winner) {
+  alert("GameOver Player"+ winner + " wins!");
 
-function renderTurn(){
-  var turnAlert = document.getElementsByClassName('turnAlert');
-  console.log(turnAlert);
-}
-/*
-// get random index
-var randIndex = Math.floor(Math.random() * (65 - 0));
-//get random cell
-var randCell = gridCells[randIndex];
-// find neighboring cells to make a ship
-var ship = randCell
-*/
+};
+//
+// function renderTurnAlert(){
+//   //getter
+//
+//   //setter
+//
+// };
+// /*
+// // get random index
+// var randIndex = Math.floor(Math.random() * (65 - 0));
+// //get random cell
+// var randCell = gridCells[randIndex];
+// // find neighboring cells to make a ship
+// var ship = randCell
+// */
